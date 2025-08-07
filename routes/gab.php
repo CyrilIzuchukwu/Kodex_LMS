7f148c6438eaa0e8137d07913e6755b7043c4e74
CyrilIzuchukwu <alexcyril34@gmail.com>
2025-08-07 15:19:41 +0100

    Fixed layout issue

--- routes/gab.php
+++ routes/gab.php
@@ -1,6 +1,4 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -34,19 +32,3 @@ Route::get('/enterVCode2', function () {
 Route::get('/forgotPassword', function () {
     return view('HTML_MARKUP.auth.forgotPassword');
 });
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
-require __DIR__ . '/gab.php';
-
-
